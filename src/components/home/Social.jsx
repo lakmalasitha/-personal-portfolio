ca7b78fe412ca9d2893959c5892d08eba13cb2c1
lakmalasitha <40794111+lakmalasitha@users.noreply.github.com>
2023-11-26 23:02:24 +0000

    portfolio home page done

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+ const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="#" className="home__social-icon" target='blank'>
+        <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="#" className="home__social-icon" target='blank'>
+        <i class="uil uil-instagram-alt"></i>
+        </a>
+
+        <a href="#" className="home__social-icon" target='blank'>
+        <i class="uil uil-github"></i>
+        </a>
+
+        <a href="#" className="home__social-icon" target='blank'>
+        <i class="uil uil-twitter"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social;
